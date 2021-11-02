c7eb56149f875ce810fed0bdce91dbcef7cb0a45
youone04 <yudi.gunaone87@gmail.com>
2021-11-02 13:55:29 +0700

    update deskripsi

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   const [key , setKey] = useState('');
   const [text , setTeks] = useState('');
 
+  const[chiper ,setChiper] = useState('');
+
   
 
   const enkripsi = () => {
@@ -64,8 +66,62 @@ function App() {
 
   }
 
+  const dekripsi = () => {
+    const data = [];
+    let n = 0;
+    let plaintext = '';
+  
+//pembentukan kunci
+    for(let i =0; i<chiper.length; i++){
+      if(chiper[i] !== '' && key[n] !== ''){
+        data[i] = key[n].toLocaleUpperCase();
+        n++;
+      }else if(key[n] !== ''){
+        data[i] = chiper[i]
+      }
+
+      if(key[n] === ''){
+        n++;
+      }
+
+      if(n===key.length){
+        n=0;
+      }
+    }
+
+    for(let i=0; i< chiper.length; i++){
+      var hasil;
+      if(alphanumeric(chiper[i])){
+       if((chiper[i].toLocaleUpperCase().charCodeAt(0)-65) - (data[i].charCodeAt(0)-65) < 0){
+          hasil = ((chiper[i].toLocaleUpperCase().charCodeAt(0)-65)-(data[i].charCodeAt(0)-65))+26;
+          // console.log(hasil)
+          console.log(hasil)
+       }else{
+        // console.log(data[i].charCodeAt(0))
+         hasil = ((chiper[i].toLocaleUpperCase().charCodeAt(0)-65)-(data[i].charCodeAt(0)-65))%26;
+         
+       }
+
+        let karakter =String.fromCharCode(hasil+65);
+        if(chiper[i] === '' || !alphanumeric(chiper[i])){
+          plaintext += chiper[i];
+        }else{
+          plaintext += karakter;
+        }
+
+
+      }else{
+        plaintext += chiper[i] 
+      }
+    }
+
+    alert(plaintext);
+
+  }
+
   return (
     <>
+    {/* enkripsi */}
     <textarea onChange={(e) => setTeks(e.target.value)}>
 
     </textarea>
@@ -73,7 +129,17 @@ function App() {
     <input onChange={(e) => setKey(e.target.value)} />
     <br/>
     <button onClick={() => enkripsi()}>enkripsi</button>
-    </>
+
+    {/* deskirpsi */}
+    <br/><br/>
+    <textarea onChange={(e) => setChiper(e.target.value)}>
+
+    </textarea>
+    <br/>
+    <input onChange={(e) => setKey(e.target.value)} />
+    <br/>
+    <button onClick={() => dekripsi()}>deksripsi</button>
+        </>
   );
 }
 
